2398c4bbf4f54766c81b5c2ca1eb8dad915ef005
Peter Sarich <scar.h13@gmail.com>
2023-09-25 09:40:10 -0700

    one color issue

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,16 +1,56 @@
-import 'package:image_editor/image_editor.dart';
 import 'package:flutter/material.dart';
+//import 'package:matrix2d/matrix2d.dart';
 
-class Home extends StatefulWidget{
+class Home extends StatefulWidget {
   @override
   _HomeState createState() => _HomeState();
 }
 
+var nCols = 31;
+var nRows = 6;
+var avab = List<List<bool>>.generate(nRows, (i) => List<bool>.generate(nCols,(j)=> true));
+//create an array with all spaces in a parking lot
+
 
 class _HomeState extends State<Home> {
+  List<Offset> rectanglePositions = [];
+
   @override
+  void initState() {
+    super.initState();
+    avab[0][0] = false;
+    drawGreenRectangles(nRows, nCols);
+
+    print(avab);
+  }
 
 
+
+  void drawGreenRectangles(int columns, int rows) {
+
+    List<double> startX = [28, 100, 170, 240, 312, 385];//offsets for creating the columns of parking spaces
+
+
+    final double startY = 15;
+    final double verticalSpacing = 14.5;
+
+
+    for(int m = 0; m < columns; m++){
+      for (int i = 0; i < rows; i++) {
+
+        // Adjust the X position for the second column
+        double y = startY + (i * verticalSpacing);
+        double x = startX[m];
+
+        rectanglePositions.add(Offset(x, y));
+
+        }
+      }
+
+    }
+
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[200],
@@ -21,19 +61,15 @@ class _HomeState extends State<Home> {
         elevation: 0,
       ),
       body: Padding(
-
         padding: const EdgeInsets.fromLTRB(0, 120.0, 0, 0),
         child: GestureDetector(
           child: CustomPaint(
             child: Image(
               image: AssetImage('assets/camosunParkingLot.jpg'),
-
             ),
-            foregroundPainter: ShapePainter(),
+            foregroundPainter: ShapePainter(rectanglePositions),
 
           ),
-
-
         ),
       ),
     );
@@ -41,24 +77,45 @@ class _HomeState extends State<Home> {
 }
 
 class ShapePainter extends CustomPainter {
-  @override
-  void paint(Canvas canvas, Size size){
+  final List<Offset> rectanglePositions;
+  final double rectWidth = 28;
+  final double rectHeight = 10;
 
+  ShapePainter(this.rectanglePositions);
+
+  @override
+  void paint(Canvas canvas, Size size) {
     var paint = Paint()
-      ..color = Colors.greenAccent
+      ..color = DetermineColor(avab)
       ..style = PaintingStyle.fill;
-    final pL = Offset(28, 15);
-    double sW = 30;
-    double sH = 10;
-    final radius = Radius.circular(4);
-
-    final rect = Rect.fromCenter(center: pL, width: sW, height: sH);
 
 
-    canvas.drawRRect(RRect.fromRectAndRadius(rect, radius), paint);
+    final double rectRadius = 4;
+
+    for (var position in rectanglePositions) {
+      final rect = Rect.fromCenter(
+        center: position,
+        width: rectWidth,
+        height: rectHeight,
+
+      );
+      canvas.drawRRect(RRect.fromRectAndRadius(rect, Radius.circular(rectRadius)), paint);
+    }
   }
+
   @override
-  bool shouldRepaint(CustomPainter oldDelegate){
+  bool shouldRepaint(CustomPainter oldDelegate) {
     return false;
   }
-}
+
+
+  Color DetermineColor(List<List<bool>> arr) {
+    if(arr[0][0]) {
+      return Colors.greenAccent;
+    }
+    else{
+      return Colors.red;
+    }
+  }
+  }
+
