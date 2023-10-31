2114b19df9a217927115269b3e0f6c562879ea76
Peter <scar.h13@gmail.com>
2023-10-30 21:07:01 -0700

    after gpt solves everything

--- lib/pages/detailed.dart
+++ lib/pages/detailed.dart
@@ -10,6 +10,10 @@ class Detailed extends StatefulWidget{
 
 class _DetailedState extends State<Detailed> {
 
+  double imageHeight = 1080;
+  double imageWidth = 1920;
+  int rotation = 0;
+
   ParkingLot? parkingLot = ParkingLot(
     lotName: "Default Lot",
     lotURL: "https://example.com/default-lot.jpg",
@@ -26,16 +30,17 @@ class _DetailedState extends State<Detailed> {
 
   void initState(){
     super.initState();
-
-
-
-    //print();
-
   }
 
 
   @override
   Widget build(BuildContext context) {
+
+    if (MediaQuery.of(context).orientation == Orientation.portrait ){
+      rotation = 1;
+    }else{
+      rotation = 0;
+    }
     return Scaffold(
       resizeToAvoidBottomInset: false,
       appBar: AppBar(
@@ -54,16 +59,24 @@ class _DetailedState extends State<Detailed> {
               parkingLot = snapshot.data;
               return Column(
                 children: [
-                  FittedBox(child: Image.network(parkingLot!.lotURL)),
+                  RotatedBox(
+                    quarterTurns: rotation,
+                    child: FittedBox(
+                      child:CustomPaint(size: Size(imageWidth, imageHeight),
+                        child: Image.network(parkingLot!.lotURL),// Set the size as per your image dimensions
+                        foregroundPainter: RectanglePainter(
+                          parkingLot!.parkingStalls,
+                          imageWidth,
+                          imageHeight,
+                        ),
+                      ),
+                    ),
+                  ),
+                  
                 ],
               );
             } else {
               return Text('Error: ${snapshot.error}');
-//Image.network(parkingLot.lotURL);
-            //CustomPaint(
-                  //size: Size(1920, 1080), // Set the size as per your requirements
-           //painter: ShapePainter(futureParkingLot.parkingStalls, futureParkingLot.totalStalls),
-             // );
             }
           },
         ),
@@ -73,3 +86,42 @@ class _DetailedState extends State<Detailed> {
 
 }
 
+class RectanglePainter extends CustomPainter {
+  final List<ParkingStalls> parkingStalls;
+  final double imageWidth;
+  final double imageHeight;
+
+  RectanglePainter(this.parkingStalls, this.imageWidth, this.imageHeight);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint()
+      ..color = Colors.black87
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 5.0;
+
+    final paint2 = Paint()
+      ..color = Colors.green
+      ..style = PaintingStyle.fill;
+
+
+    for (var stall in parkingStalls) {
+      final x = (stall.x / imageWidth) * size.width;
+      final y = (stall.y / imageHeight) * size.height;
+      final rect = Rect.fromPoints(Offset(x, y), Offset(x + 35, y + 80)); // Adjust the rectangle size as needed
+      final x2 = ((stall.x + 2) / imageWidth) * size.width ;
+      final y2 = ((stall.y + 2)  / imageHeight) * size.height ;
+      final rect2 = Rect.fromPoints(Offset(x2, y2), Offset(x2 + 31, y2 + 76));
+      canvas.drawRect(rect, paint);
+      canvas.drawRect(rect2, paint2);
+
+
+    }
+
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return false;
+  }
+}
